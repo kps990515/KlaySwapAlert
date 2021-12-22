22a976babc1038fb563229ebba4ba1a5a58f939c
kps990515 <31429892+kps990515@users.noreply.github.com>
2021-12-22 19:41:45 +0900

    dd

--- src/main/java/com/example/klayswapalert/KlayController.java
+++ src/main/java/com/example/klayswapalert/KlayController.java
@@ -77,7 +77,7 @@ public class KlayController {
             RestTemplate restTemplate = new RestTemplate(factory);
 
             HttpHeaders header = new HttpHeaders();
-            header.add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36");
+            header.add("User-Agent", "Appication");
 
             HttpEntity<?> entity = new HttpEntity<>(header);
 
